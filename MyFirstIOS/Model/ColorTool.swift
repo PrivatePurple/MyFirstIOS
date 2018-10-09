c1fb6b2a6d825bf49e20f3e6fe65278c5b085534
Keenan <Keenanking555@gmail.com>
2018-10-09 12:20:48 -0600

    added Model group and changed filenames

---
+++ MyFirstIOS/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  MyFirstIOS
+//
+//  Created by Davis, Keenan on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
